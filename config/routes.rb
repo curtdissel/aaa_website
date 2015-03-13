05aad1146a5bbbbfa3a78a85593b299fe9ce63d1
curtdissel <curtdissel@gmail.com>
2015-03-13 17:13:18 -0400

    ready to push to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   devise_for :members
   resources :events
 
-  get 'aaa_core/index'
-  get 'aaa_core/events'
-  get 'aaa_core/shop'
-  get 'aaa_core/discussion'
+#  get 'aaa_core/index'
+#  get 'aaa_core/events'
+#  get 'aaa_core/shop'
+#  get 'aaa_core/discussion'
 #  match "/index" => 'aaa_core#index', via: :get
 #  match "/shop" => 'aaa_core#shop', via: :get
 #  match "/discussion" => 'aaa_core#discussion', via: :get
